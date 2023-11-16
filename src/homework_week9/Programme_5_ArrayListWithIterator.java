4d5e024d0b47b4e6c641c6c0ee9f929cbdbd6400
prachigoyalgithub <prachigoyal.new@gmail.com>
2023-11-16 23:19:43 +0000

    java_homework_writing_week9

---
+++ src/homework_week9/Programme_5_ArrayListWithIterator.java
@@ -0,0 +1,33 @@
+package homework_week9;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * Created by Jay Vaghani
+ */
+/*
+Write a Java program to iterate through all elements in an array list using Iterator.
+ */
+
+
+public class Programme_5_ArrayListWithIterator {
+    public static void main(String[] args) {
+        ArrayList<String> fruitList = new ArrayList<>();
+        fruitList.add("APPLE");
+        fruitList.add("BANANA");
+        fruitList.add("GRAPE");
+        fruitList.add("MANGO");
+        fruitList.add("KIWI");
+        fruitList.add("ORANGE");
+        fruitList.add("BLACKBERRY");
+        fruitList.add("STRAWBERRY");
+        fruitList.add("CHERRY");
+
+        ListIterator<String> iterate = fruitList.listIterator();
+        while (iterate.hasNext()) {
+            System.out.print(iterate.next() + ", ");
+        }
+    }
+}
+
